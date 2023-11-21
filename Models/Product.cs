5892ae21919efabb8cfabbfae93564760062b570
mike focken <mikefocken@gmail.com>
2023-11-21 12:57:18 -0600

    initail commit, before migration

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product_ReviewWebAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        [ForeignKey("Review")]
+        public int ReviewId { get; set; }
+        public Review Review { get; set;}
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product_ReviewWebAPI.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+
+
+        public Product Product { get; set; }
+
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+       
+
+
+
+
+    }
+}
